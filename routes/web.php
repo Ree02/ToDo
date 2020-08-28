eadfb1ad2e384a279b8aac6cfcdcc162128143d0
Ree02 <ri.02kh.aax@gmail.com>
2020-08-28 17:59:37 +0900

    タスク追加時の画面遷移デバッグ

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/', 'HomeController@index')->name('home');
 
     //タスク一覧ページ表示
-    Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+    Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
 
     //フォルダ作成ページ表示
     Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
@@ -27,14 +27,14 @@ Route::group(['middleware' => 'auth'], function() {
     Route::post('/folders/create', 'FolderController@create');
 
     //タスク作成ページ表示
-    Route::get('/folders/{id}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
+    Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
     //タスク作成処理実効
-    Route::post('/folders/{id}/tasks/create', 'TaskController@create');
+    Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
 
     //タスク編集ページ表示
-    Route::get('/folders/{id}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
+    Route::get('/folders/{folder}/tasks/{task}/edit', 'TaskController@showEditForm')->name('tasks.edit');
     //タスク編集処理実効
-    Route::post('/folders/{id}/tasks/{task_id}/edit', 'TaskController@edit');
+    Route::post('/folders/{folder}/tasks/{task}/edit', 'TaskController@edit');
 });
 
 //会員登録
